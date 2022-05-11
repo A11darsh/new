fa931664d1d3b9a8bbdc66c43f0a92795f95fa8c
adarsh <adarshmishra5432@gmail.com>
2022-05-11 18:11:43 +0530

    j unit testing

---
+++ src/com/zensar/client/Client.java
@@ -0,0 +1,15 @@
+package com.zensar.client;
+
+import com.zensar.code.Addition;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Addition a= new Addition();
+	    System.out.println(a.add(10, 5));
+	    System.out.println(a.sub(4, 3));
+	    System.out.println(a.mul(2, 3));
+	    System.out.println(a.div(2, 2));
+	}
+
+}
